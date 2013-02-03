899c18a066d1e24ce3bc6fbb1c62bf6fb6c8b38f
Sean Sabbage <sgsabbage@gmail.com>
2013-02-03 23:40:14 +0000

    User PHPUnit tests in place

--- src/SGS/Model/User.php
+++ src/SGS/Model/User.php
@@ -2,13 +2,17 @@
 namespace SGS\Model;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Table(name="user")
  * @ORM\Entity
  */
-class User {
+class User implements UserInterface{
 
+    const TYPE_ENGINEER   = 1;
+    const TYPE_REQUESTER  = 2;
+    const TYPE_DISPATCHER = 3;
     /**
      * @ORM\Column(type="integer")
      * @ORM\Id
@@ -20,6 +24,29 @@ class User {
      * @ORM\Column(type="string", length=255, unique=true)
      */
     protected $email;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    protected $salt;
+
+    /**
+     * @ORM\Column(type="string", length=1024)
+     */
+    protected $password;
+
+    /**
+     * @ORM\Column(type="integer", length=1)
+     */
+    protected $type;
+
+    /**
+     * Creates the salt when constructed
+     */
+    public function __construct()
+    {
+        $this->salt = md5(uniqid(null,true));
+    }
 
     /**
      * Get id
@@ -52,5 +79,80 @@ class User {
     public function getEmail()
     {
         return $this->email;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Sets the user's hashed password
+     * 
+     * @param string $password
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set type
+     *
+     * @param integer $type
+     * @return User
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return integer 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function eraseCredentials()
+    {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getRoles()
+    {
+        return array();
     }
 }
--- src/SGS/Tests/Model/UserTest.php
+++ src/SGS/Tests/Model/UserTest.php
@@ -23,4 +23,29 @@ class UserTest extends \PHPUnit_Framework_TestCase
         $this->user->setEmail($email);
         $this->assertEquals($email,$this->user->getEmail());
     }
+
+    public function testEmailIsUsername()
+    {
+        $email = 'foo@foo.com';
+        $this->user->setEmail($email);
+        $this->assertEquals($email,$this->user->getUsername());
+    }
+
+    public function testSetGetPassword()
+    {
+        $password = "Pass";
+        $this->user->setPassword($password);
+        $this->assertEquals($password,$this->user->getPassword());
+    }
+
+    public function testSaltIsNotNull()
+    {
+        $this->assertNotNull($this->user->getSalt());
+    }
+
+    public function testSetGetType()
+    {
+        $this->user->setType(User::TYPE_ENGINEER);
+        $this->assertEquals(User::TYPE_ENGINEER,$this->user->getType());
+    }
 }

425362ee30f585c01cd3612a6aa4f9fa9f1febc7
Sean Sabbage <sgsabbage@gmail.com>
2013-02-04 11:01:06 +0000

    Started adding the required parts for login

---
+++ src/SGS/Bundle/AppBundle/Controller/Dispatch/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+namespace SGS\Bundle\AppBundle\Controller\Dispatch;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * @Route("/dispatch")
+ */
+class DashboardController
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+
+    }
+}
